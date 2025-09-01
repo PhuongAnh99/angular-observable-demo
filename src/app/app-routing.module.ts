5b7d27ffe10d244aace33a51bfc68ea2e44575b5
PhuongAnh <phuonganhnthi98@gmail.com>
2025-09-01 19:02:01 +0700

    Update App component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./features/users/users.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./features/posts/posts.module').then((m) => m.PostsModule),
+  },
+  {
+    path: 'comments',
+    loadChildren: () =>
+      import('./features/comments/comments.module').then(
+        (m) => m.CommentsModule
+      ),
+  },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: '**', redirectTo: 'users' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
